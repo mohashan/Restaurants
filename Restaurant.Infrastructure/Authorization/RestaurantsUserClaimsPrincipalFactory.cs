9bf2a76116423471221b2ffea1b608cdf3c87c9f
Mohashan <msh200x@gmail.com>
2024-04-09 08:45:12 +0330

    Add Custom claim to token through UserClaimPrincipalFactory

---
+++ Restaurant.Infrastructure/Authorization/RestaurantsUserClaimsPrincipalFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Restaurants.Domain.Entities;
+using System.Security.Claims;
+
+namespace Restaurants.Infrastructure.Authorization;
+
+public class RestaurantsUserClaimsPrincipalFactory(UserManager<User> userManager,
+    RoleManager<IdentityRole> roleManager,
+    IOptions<IdentityOptions> options) : UserClaimsPrincipalFactory<User,IdentityRole>(userManager,roleManager,options)
+{
+    public override async Task<ClaimsPrincipal> CreateAsync(User user)
+    {
+        var claims = await GenerateClaimsAsync(user);
+
+        if(user.Nationality is not null)
+            claims.AddClaim(new Claim("Nationality",user.Nationality));
+
+        if (user.DateOfBirth is not null)
+            claims.AddClaim(new Claim("DateOfBirth", user.DateOfBirth.Value.ToString("yyyy-MM-dd")));
+
+        return new ClaimsPrincipal(claims);
+    }
+}
--- Restaurant.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurant.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.Users;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Authorization;
 using Restaurants.Infrastructure.Persistence;
 using Restaurants.Infrastructure.Repositories;
 using Restaurants.Infrastructure.Seeders;
@@ -22,6 +23,7 @@ public static class ServiceCollectionExtensions
 
         services.AddIdentityApiEndpoints<User>()
             .AddRoles<IdentityRole>()
+            .AddClaimsPrincipalFactory<RestaurantsUserClaimsPrincipalFactory>()
             .AddEntityFrameworkStores<RestaurantsDbContext>();
 
         services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
