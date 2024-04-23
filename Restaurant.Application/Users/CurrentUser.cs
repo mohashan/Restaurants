4ffd62a7be76f5008f11cb78f4786091e1a47fa9
Mohashan <msh200x@gmail.com>
2024-04-23 16:18:29 +0330

    Add Test Project

--- Restaurant.Application/Users/CurrentUser.cs
+++ Restaurant.Application/Users/CurrentUser.cs
@@ -6,11 +6,15 @@ using System.Threading.Tasks;
 
 namespace Restaurants.Application.Users;
 
-public record CurrentUser(string Id,
+public class CurrentUser(string Id,
     string Email, 
     IEnumerable<string> Roles, 
     string? Nationality, 
     DateOnly? DateOfBirth)
 {
+    public string Id { get; set; } = Id;
+    public string Email { get; set; } = Email;
+    public string? Nationality { get; set; } = Nationality;
+    public DateOnly? DateOfBirth { get; set; } = DateOfBirth;
     public bool IsInRole(string role) => Roles.Contains(role);
 }
