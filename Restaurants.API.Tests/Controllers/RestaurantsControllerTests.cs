0d5a41ba4ac5fdfae28041af105ca9030894f8c3
Mohashan <msh200x@gmail.com>
2024-05-01 16:59:32 +0330

    Add GetAllRestaurant integration test

---
+++ Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Xunit;
+
+namespace Restaurants.API.Controllers.Tests;
+
+public class RestaurantsControllerTests:IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public RestaurantsControllerTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact()]
+    public async void GetAll_ForValidRequest_Returns200Ok()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var result = await client.GetAsync("/api/Restaurants?PageNumber=1&PageSize=5");
+
+        // Assert
+        result.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+    }
+}
